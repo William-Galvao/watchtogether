cf9dc6dca97e7533af6ed2868c4845d372782e35
Elizeu Santos <elyzeu.tec@gmail.com>
2021-11-22 18:04:03 -0300

    Em desenvolvimento

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 
+
 function App() {
   return (
     <div className="App">
       <div className="bg-dark">
         <Routes>
           <Route element={<Home />} path="/" />
-
 
         </Routes>
 
