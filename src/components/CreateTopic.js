d4eff5b35597541cd8db26bb4926076326948f7e
Elizeu Santos <elyzeu.tec@gmail.com>
2021-11-23 18:01:52 -0300

    create

--- src/components/CreateTopic.js
+++ src/components/CreateTopic.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export default function CreateTopic() {
+    const navigate = useNavigate();
     const [topic, setTopic] = useState({
         title: "",
         data: "",
@@ -10,45 +15,56 @@ export default function CreateTopic() {
 
         setTopic({
             ...topic,
-            [event.target.name]: event.target.value,
+            [event.target.name] : event.target.value,
         });
 
     }
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        try {
+            await axios.post("https://ironrest.herokuapp.com/watchtogether", topic);
+            navigate("/");
+        } catch (error) {
+            console.error(error.response.data);
+        }
+    }
 
     return (
-        <form>
-            <label htmlFor="title">Titulo:</label>
-            <input
-                id="title"
-                value={topic.title}
-                type="text"
-                onChange={handleChange}
-                name="title" />
+    <form>
+        <label htmlFor="title">Titulo:</label>
+        <input 
+            id="title" 
+            value={topic.title} 
+            type="text" 
+            onChange={handleChange} 
+            name="title" />
 
-            <label htmlFor="data">Data:</label>
-            <input
-                id="data"
-                value={topic.data}
-                type="number"
-                onChange={handleChange}
-                name="data" />
+        <label htmlFor="data">Data:</label>
+        <input 
+            id="data"
+            value={topic.data}
+            type="date"
+            onChange={handleChange}
+            name="data" />
 
-            <label htmlFor="video">Streaming:</label>
-            <input
-                id="video"
-                value={topic.video}
-                type="video"
-                onChange={handleChange}
-                name="video" />
+        <label htmlFor="video">Streaming:</label>
+        <input 
+            id="video" 
+            value={topic.video}
+            type="video"
+            onChange={handleChange}
+            name="video" />
 
-            <label htmlFor="audio">Voip:</label>
-            <input
-                id="audio"
-                value={topic.audio}
-                type="audio"
-                onChange={handleChange}
-                name="audio" />
-
-        </form>
-    )
+        <label htmlFor="audio">Voip:</label>
+        <input 
+            id="audio" 
+            value={topic.audio}
+            type="audio"
+            onChange={handleChange}
+            name="audio" />
+        
+    </form>
+    );
 }
