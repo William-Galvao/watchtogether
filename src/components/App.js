c0ac21c2d8fccc7c6c6a81f37a9bf22c83102957
William-Galvao <wsg.online@gmail.com>
2021-11-24 09:40:50 -0300

    ale estragando o codigo

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 <<<<<<< HEAD
 import CreateSession from './CreateSession';
-=======
-import CreateTopic from './CreateTopic';
->>>>>>> cc36d297a7a762957a319a1b13b0f610e32dce33
+
 
 
 
@@ -15,11 +13,8 @@ function App() {
       <div>
         <Routes>
           <Route element={<Home />} path="/" />
-<<<<<<< HEAD
           <Route element={<CreateSession />} path="/createsession" />
-=======
-          <Route element={<CreateTopic />} path="/createtopic" />
->>>>>>> cc36d297a7a762957a319a1b13b0f610e32dce33
+
         </Routes>
       </div>
     </div>
