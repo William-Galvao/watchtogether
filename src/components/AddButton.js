16464fcd49be938ac50e5e021821f6a45fcc8c57
Elizeu Santos <elyzeu.tec@gmail.com>
2021-11-24 15:07:28 -0300

    EditSession

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,5 +1,6 @@
 export default function AddButton() {
     return (
+        
         <button type="button" className="btn btn-outline-warning">Create a new movie session!</button>
     )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<CreateTopic />} path="/createtopic" />
+        
         </Routes>
       </div>
     </div>
