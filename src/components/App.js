232329a36402b9a38a35dd2f2258c90782ddd568
William-Galvao <wsg.online@gmail.com>
2021-11-24 09:05:35 -0300

    adjust submit button

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import CreateSession from './CreateSession';
 
 
 
@@ -10,7 +11,7 @@ function App() {
       <div>
         <Routes>
           <Route element={<Home />} path="/" />
-
+          <Route element={<CreateSession />} path="/createsession" />
         </Routes>
       </div>
     </div>
